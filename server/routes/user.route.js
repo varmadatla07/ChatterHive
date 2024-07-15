df942af84dc2e75f4b5e44d0021d6cecbb384d15
varmadatla07 <varmadatla07@gmail.com>
2024-07-15 13:32:03 +0530

    changemade

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,3 +1,78 @@
+// const router = require("express").Router();
+// const passport = require("passport");
+// const useragent = require("express-useragent");
+// const requestIp = require("request-ip");
+
+// const {
+//   addUser,
+//   signin,
+//   logout,
+//   refreshToken,
+//   getModProfile,
+//   getUser,
+//   updateInfo,
+// } = require("../controllers/user.controller");
+
+// const {
+//   getPublicUsers,
+//   followUser,
+//   getPublicUser,
+//   unfollowUser,
+//   getFollowingUsers,
+// } = require("../controllers/profile.controller");
+
+// const {
+//   addUserValidator,
+//   addUserValidatorHandler,
+// } = require("../middlewares/users/usersValidator");
+
+// const { sendVerificationEmail } = require("../middlewares/users/verifyEmail");
+// const {
+//   sendLoginVerificationEmail,
+// } = require("../middlewares/users/verifyLogin");
+
+// const avatarUpload = require("../middlewares/users/avatarUpload");
+// const {
+//   signUpSignInLimiter,
+//   followLimiter,
+// } = require("../middlewares/limiter/limiter");
+
+// const decodeToken = require("../middlewares/auth/decodeToken");
+// const requireAuth = passport.authenticate("jwt", { session: false }, null);
+
+// router.get("/public-users/:id", requireAuth, decodeToken, getPublicUser);
+// router.get("/public-users", requireAuth, decodeToken, getPublicUsers);
+// router.get("/moderator", requireAuth, decodeToken, getModProfile);
+// router.get("/following", requireAuth, decodeToken, getFollowingUsers);
+// router.get("/:id", requireAuth, getUser);
+
+// router.post(
+//   "/signup",
+//   signUpSignInLimiter,
+//   avatarUpload,
+//   addUserValidator,
+//   addUserValidatorHandler,
+//   addUser,
+//   sendVerificationEmail
+// );
+// router.post("/refresh-token", refreshToken);
+// router.post(
+//   "/signin",
+//   signUpSignInLimiter,
+//   requestIp.mw(),
+//   useragent.express(),
+//   signin,
+//   sendLoginVerificationEmail
+// );
+// router.post("/logout", logout);
+
+// router.put("/:id", requireAuth, decodeToken, updateInfo);
+
+// router.use(followLimiter);
+// router.patch("/:id/follow", requireAuth, decodeToken, followUser);
+// router.patch("/:id/unfollow", requireAuth, decodeToken, unfollowUser);
+
+// module.exports = router;
 const router = require("express").Router();
 const passport = require("passport");
 const useragent = require("express-useragent");
@@ -40,11 +115,17 @@ const {
 const decodeToken = require("../middlewares/auth/decodeToken");
 const requireAuth = passport.authenticate("jwt", { session: false }, null);
 
+// Existing routes
 router.get("/public-users/:id", requireAuth, decodeToken, getPublicUser);
 router.get("/public-users", requireAuth, decodeToken, getPublicUsers);
 router.get("/moderator", requireAuth, decodeToken, getModProfile);
 router.get("/following", requireAuth, decodeToken, getFollowingUsers);
 router.get("/:id", requireAuth, getUser);
+
+// Add a GET route for /signin for testing purposes
+router.get("/signin", (req, res) => {
+  res.status(200).json({ message: "Sign-in page" });
+});
 
 router.post(
   "/signup",
